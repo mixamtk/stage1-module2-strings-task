a0e8ffb144cad68ef9b0bb33933317ed5cf371f0
mixamtk <70889784+mixamtk@users.noreply.github.com>
2022-11-21 15:36:21 +0300

    Update StringSplitter.java

--- src/main/java/com/epam/mjc/StringSplitter.java
+++ src/main/java/com/epam/mjc/StringSplitter.java
@@ -14,9 +14,24 @@ public class StringSplitter {
      */
     public List<String> splitByDelimiters(String source, Collection<String> delimiters) {
         String replacement = " ";
-        for (String delimiter : delimiters) {
-            source = source.replaceAll(delimiter,replacement);
+        int lastPos = -1;
+        boolean isDelimiter = false;
+        char[] arrSource = source.toCharArray();
+        char charPos;
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < source.length(); i++){
+            charPos = arrSource[i];
+            for (String delimiter : delimiters) {
+                if (delimiter.charAt(0) == charPos) {
+                    if (lastPos != i-1) {
+                        stringBuilder.append(replacement);
+                    }
+                    lastPos = i;
+                    isDelimiter = true;
+                }
             }
-        return List.of(source.split(replacement));
-    }
+            if (!isDelimiter)  stringBuilder.append(charPos);
+            isDelimiter = false;
+        }
+        return List.of(stringBuilder.toString().split(replacement));
 }
